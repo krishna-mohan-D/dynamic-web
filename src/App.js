7263f86c7dd63ad07935a4fe61c4afcc3143bc44
donavallikrishnamohan <krishnamohan.v@etvbharat.com>
2021-03-21 16:26:45 +0530

    f

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar'
 import {BrowserRouter as Router , Switch , Route } from 'react-router-dom'
 import Home from './components/pages/Home';
-
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
  function App() {
   return (
     <>
@@ -11,10 +13,12 @@ import Home from './components/pages/Home';
          <Switch>
             <Route path="/" exact component={Home}/>
 
-            
+            <Route path='/Services' component={Services} />
+          <Route path='/Products' component={Products} />
+          <Route path='/SignUp' component={SignUp} />
          </Switch>
       </Router>
-              
+            
       
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ function Navbar() {
                             </Link>
                        </li>
                        <li className='nav-item'>
-                            <Link to="/services" className='nav-links' onClick={closeMobileMenu}>
+                            <Link to="/Services" className='nav-links' onClick={closeMobileMenu}>
                                     services 
                             </Link>
                        </li>
